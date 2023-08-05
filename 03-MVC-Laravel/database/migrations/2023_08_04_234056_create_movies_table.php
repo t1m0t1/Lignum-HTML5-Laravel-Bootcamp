b76c5ef25c3a883656d7e9b1b2ff15113ebcafc4
Tarchini Matias Nicolas <96513084+t1m0t1@users.noreply.github.com>
2023-08-05 01:11:21 -0300

    [ADD] ejercicio 3.1 y 3.2

---
+++ 03-MVC-Laravel/database/migrations/2023_08_04_234056_create_movies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->year('year');
+            $table->float('duration');
+            $table->text('synopsis');
+            $table->foreignId('mainActorId');
+            $table->timestamps();
+
+            $table->foreign('mainActorId')->references('id')->on('actors');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('movies');
+    }
+
+};
