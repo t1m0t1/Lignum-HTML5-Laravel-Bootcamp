8aef193116cc8a870b96c53da6bb3bba3a22f907
Tarchini Matias Nicolas <96513084+t1m0t1@users.noreply.github.com>
2023-08-10 01:47:51 -0300

    [ADD] factory para cargar datos de prueba

---
+++ 03-MVC-Laravel/database/factories/MovieFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Movie>
+ */
+class MovieFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake() -> sentence(4),
+            'year'=> fake() -> dateTime(),
+            'duration'=> fake()-> randomFloat(2,60,120),
+            'synopsis'=> fake()-> paragraph(),
+        ];
+    }
+}
--- 03-MVC-Laravel/database/seeders/DatabaseSeeder.php
+++ 03-MVC-Laravel/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Actor;
+use App\Models\Movie;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,10 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+        Actor::factory(60)->create();
+        Movie::factory(20)
+                ->for(Actor::factory(),'mainActor')
+                ->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
