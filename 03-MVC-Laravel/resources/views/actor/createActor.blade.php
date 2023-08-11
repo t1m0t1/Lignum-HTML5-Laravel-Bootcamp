a9785f78dc9b8b38ee3812dd068bcef2b506bc58
Tarchini Matias Nicolas <96513084+t1m0t1@users.noreply.github.com>
2023-08-11 02:30:10 -0300

    vistas crud actor

--- 03-MVC-Laravel/resources/views/actor/createActor.blade.php
+++ 03-MVC-Laravel/resources/views/actor/createActor.blade.php
@@ -1,25 +1,45 @@
-
 @extends('layout/template')
 
-@section('title','Create new Actor')
-    
+@section('title', 'Create new Actor')
+
 @section('content')
-<main>
-   @if ($errors -> any())
-       <ul>
-        @foreach ($errors as $error)
-        <li> {{$error}} </li>
-        @endforeach
-       </ul>
-   @endif
+    <main>
+        @if ($errors->any())
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li> {{ $error }} </li>
+                @endforeach
+            </ul>
+        @endif
+            <section class="container bg-dark bg-gradient pb-5">
 
-   <form action="{{url('actor')}}" method="POST">
-    @csrf
-        <label for="name">Name Actor</label>
-        <input type="text" name="name" id="name" value="{{old('name')}}">
-        <label for="dateActor">Fecha de Nacimiento</label>
-        <input type="date" name="dateActor" id="dateActor" value="{{old('dateActor')}}">
-        <button type="submit">Create</button>
-    </form>
+            <form action="{{ url('actor') }}" method="POST" enctype="multipart/form-data">
+                    @csrf
+            <label class="text-light" for="imageActor">Photo</label>
+            <img class="d-block mb-3 mt-3" src="" alt="" id="imagePreview" style='height: 10vh'>
+            <input class="form-control" type="file" name="imageActor" accept="image/*" onchange="showFile(event)">
+            
+            <label class="text-light" for="name">Name Actor</label>
+            <input class="form-control" type="text" name="name" id="name" value="{{ old('name') }}">
+            
+            <label class="text-light" for="dateActor">Date of birth</label>
+            <input  class="form-control" type="date" name="dateActor" id="dateActor" value="{{ old('dateActor') }}">
+            <div class="d-grid gap-2">
+                <button class="btn btn-primary mt-5" type="submit">Edit</button>
+            </div>
+        </form>
+    </section>
 
-</main>
+    </main>
+    <script>
+        function showFile(event){
+            var input = event.target;
+            var reader = new FileReader();
+            reader.onload = function (){
+                var dataURL = reader.result;
+                var output = document.getElementById('imagePreview');
+                output.src = dataURL;
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    </script>
---
+++ 03-MVC-Laravel/resources/views/actor/editActor.blade.php
@@ -0,0 +1,55 @@
+@extends('layout/template')
+
+@section('title', 'Edit Actor')
+
+@section('content')
+    <main>
+
+        <h1 class="text-light text-center">Edit Actor</h1>
+        @if ($errors->any())
+            <ul>
+                @foreach ($errors as $error)
+                    <li> <p class="text-light">{{ $error }}</p> </li>
+                @endforeach
+            </ul>
+        @endif
+        <section class="container bg-dark bg-gradient pb-5">
+            <form action="{{ url('actor/'.$actor->id) }}" method="post" enctype="multipart/form-data">
+                @method('put')
+               
+                @csrf
+
+                <input type="hidden" name="hiddenId" value="{{ $actor->id }}">
+
+                <label class="text-light" for="imageActor">Photo</label>
+                <img class="d-block mb-3 mt-3" src='{{ asset('images/' . $actor->imageActor) }}' alt=""
+                    id="imagePreview" style='height: 10vh'>
+                <input type="hidden" name="hiddenImageActor" value="{{ $actor->imageActor }}">
+                <input class="form-control" type="file" name="imageActor" accept="image/*" onchange="showFile(event)">
+
+                <label class="text-light" for="name">Name Actor</label>
+                <input class="form-control" type="text" name="name" id="name" value="{{ $actor->name }}">
+
+                <label class="text-light" for="dateActor">Date of birth</label>
+                <input class="form-control" type="date" name="dateActor" id="dateActor"
+                    value="{{ $actor->date_of_birth }}">
+
+                <div class="d-grid gap-2">
+                    <button class="btn btn-primary mt-5" type="submit">Edit</button>
+                </div>
+            </form>
+
+        </section>
+    </main>
+    <script>
+        function showFile(event) {
+            var input = event.target;
+            var reader = new FileReader();
+            reader.onload = function() {
+                var dataURL = reader.result;
+                var output = document.getElementById('imagePreview');
+                output.src = dataURL;
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    </script>
